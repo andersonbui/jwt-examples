c35829fb9f6d5b8486285d13aa9e0edd9016ab6b
Anderson Buitron <andersonb@hotmail.es>
2023-06-28 14:12:40 -0500

    [ADD] Se agraga verificacion con llave publica

--- node/sign-and-verifying/utils/token.js
+++ node/sign-and-verifying/utils/token.js
@@ -2,9 +2,12 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
+import fs from "node:fs";
 import jwt from "jsonwebtoken";
 
 const SECRET = process.env.SECRET;
+const PRIVATE_KEY_PATH=process.env.PRIVATE_KEY_PATH;
+const PUBLIC_KEY_PATH=process.env.PUBLIC_KEY_PATH;
 const ONE_MINUTE_IN_MILLISECONDS = 60 * 1000
 
 export const signToken = (user) => {
@@ -14,10 +17,19 @@ export const signToken = (user) => {
     exp: Date.now() + ONE_MINUTE_IN_MILLISECONDS,
   };
 
+  if(PRIVATE_KEY_PATH){
+    const privateKey = fs.readFileSync(PRIVATE_KEY_PATH, 'utf8');
+    return jwt.sign(payload, privateKey, { algorithm: "RS256" });
+  }
+
   return jwt.sign(payload, SECRET)
 };
 
 export const verifyToken = (token) => {
+  if(PRIVATE_KEY_PATH){
+    const publicKey = fs.readFileSync(PUBLIC_KEY_PATH, 'utf8');
+    return jwt.verify(token, publicKey)  
+  }
   let data = jwt.verify(token,SECRET)
   return data
 };
