11e21947e0470046038d2e95fc747f00fdc4acdb
Anderson Buitron <andersonb@hotmail.es>
2023-06-28 12:53:44 -0500

    [ADD] primer commit con proyecto inicicla de nodejs

---
+++ node/sign-and-verifying/utils/token.js
@@ -0,0 +1,28 @@
+// Load environment variables
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import jwt from "jsonwebtoken";
+
+const SECRET = process.env.SECRET;
+const ONE_MINUTE_IN_MILLISECONDS = 60 * 1000
+
+export const signToken = (user) => {
+  const payload = {
+    sub: user.id,
+    name: user.fullname,
+    exp: Date.now() + ONE_MINUTE_IN_MILLISECONDS,
+  };
+
+  return jwt.sign(payload, SECRET)
+};
+
+export const verifyToken = (token) => {
+  return {exp: 122222222222555555};
+};
+
+export const validateExpiration = (payload) => {
+  if (Date.now() > payload.exp) {
+    throw new Error("Token expired");
+  }
+};
